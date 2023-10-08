2dbc68aec75c202fecce01bbdbc10cbc141a56bc
RowanAshour <rokamino@hotmail.com>
2023-10-08 05:20:30 -0700

    4&5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,21 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase, excluding 'q' and 'e', using putchar
+ * Description: Prints the alphabet in lowercase, excluding 'q' and 'e',
+ *              using putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+    char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'q' && letter != 'e')
-			putchar(letter);
-	}
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+            putchar(letter);
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,18 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single-digit numbers of base 10 starting from 0 using putchar
+ * Description: Prints all single-digit numbers of base 10 starting from 0
+ *              using putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number;
+    int number;
 
-	for (number = 0; number < 10; number++)
-		putchar(number + '0');
+    for (number = 0; number < 10; number++)
+        putchar(number + '0');
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
-}
+    return (0);
+}}
