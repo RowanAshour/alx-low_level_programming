067a9b9dadeb705994940c413190d6619ed69c0c
RowanAshour <rokamino@hotmail.com>
2023-10-11 10:47:52 -0700

    16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned int fib1 = 1, fib2 = 2, fib_next, count;
+
+	printf("%u, %u", fib1, fib2);
+
+	for (count = 3; count <= 98; count++)
+	{
+		fib_next = fib1 + fib2;
+		printf(", %u", fib_next);
+
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
