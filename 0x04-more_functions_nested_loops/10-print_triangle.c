d1993af43b819781a0da5e4e8685d1e02ade1de8
RowanAshour <rokamino@hotmail.com>
2023-10-12 12:19:16 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,24 +3,32 @@
 /**
  * print_triangle - Prints a triangle of a given size
  * @size: The size of the triangle
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
-int i, j;
+	int i, j;
 
-if (size <= 0)
-{
-_putchar('\n');
-return;
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-for (i = 0; i < size; i++)
-{
-for (j = 0; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			_putchar(' ');
+		}
+
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
 }
 
