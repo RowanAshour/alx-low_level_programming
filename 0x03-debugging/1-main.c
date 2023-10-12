843ab1992602fe5e9ace06b3210a8cd5ccf61240
RowanAshour <rokamino@hotmail.com>
2023-10-12 05:16:16 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,7 @@
 /**
  * main - Causes an infinite loop
  *
- * Return: Always 0
+ * Return: i0
  */
 int main(void)
 {
@@ -13,13 +13,11 @@ int main(void)
 
 	i = 0;
 
-	/*
-	*  while (i < 10)
-	{
-		putchar(i);
-	}
-	*/
-
+//	  while (i < 10)
+//	{
+//		putchar(i);
+//	}
+	
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
