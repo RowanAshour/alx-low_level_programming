ba8edc2eb833918c0ebd409dde52661d26358ccd
RowanAshour <rokamino@hotmail.com>
2023-10-19 11:22:14 -0700

    9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,15 @@
  */
 void print_number(int n)
 {
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-	}
-
-	if (n / 10)
-	{
-		print_number(n / 10);
 	}
 
 	_putchar((n % 10) + '0');
