e3ea332298e1917542465313f64a5ae0a811e242
RowanAshour <rokamino@hotmail.com>
2023-10-19 11:54:03 -0700

    9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,10 +6,17 @@
  *
  * Description: This function prints an integer using the _putchar function.
  * If the number is negative, it prints a '-' character first.
- * It recursively divides the number by 10 until it becomes 0, then prints each digit.
+ * It recursively divides the number by 10 until it becomes 0,
+ * then prints each digit.
  */
 void print_number(int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
