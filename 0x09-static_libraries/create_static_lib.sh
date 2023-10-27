e0bb1f2313d56f2921853ae6b5a6e9f77d1aa6b6
RowanAshour <rokamino@hotmail.com>
2023-10-27 03:23:17 -0700

    1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,7 +2,7 @@
 # This script compiles all .c files in the current directory into .o files and then creates a static library from them.
 
 # Compile all .c files into .o files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
+gcc -Wall -Wextra -pedantic -c *.c
 
 # Create a static library from all .o files
 ar -rc liball.a *.o
