b17276f8f1ae19ca69f96f051d35ce14d0a644ce
RowanAshour <rokamino@hotmail.com>
2023-11-02 05:18:12 -0700

    5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,58 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Multiplies two positive numbers.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
+ * is_digit - checks if a string only contains digits
+ * @str: string to check
  *
- * Return: If the number of arguments is incorrect or one of the numbers
- *         contains non-digits - 98. Otherwise - 0.
+ * Return: 1 if str only contains digits, and 0 otherwise
+ */
+int is_digit(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: 0 if function succeeded, 1 if function failed
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, product;
+	int num1, num2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
+	}
+
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	if (num1 == 0 || num2 == 0)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	product = num1 * num2;
-	printf("%d\n", product);
-
+	printf("%d\n", num1 * num2);
 	return (0);
 }
