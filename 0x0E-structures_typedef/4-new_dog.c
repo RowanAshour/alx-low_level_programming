1cd453663495f5cb671f8ccad5538e80d598c7fe
RowanAshour <rokamino@hotmail.com>
2023-11-06 13:26:16 -0800

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,36 +12,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *name_copy, *owner_copy;
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
-    /* Allocate memory for the new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	/* Allocate memory for the new dog */
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    /* Allocate memory and copy the name */
-    name_copy = strdup(name);
-    if (name_copy == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	/* Allocate memory and copy the name */
+	name_copy = strdup(name);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    /* Allocate memory and copy the owner */
-    owner_copy = strdup(owner);
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(new_dog);
-        return (NULL);
-    }
+	/* Allocate memory and copy the owner */
+	owner_copy = strdup(owner);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
 
-    /* Initialize the fields of the new dog */
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
+	/* Initialize the fields of the new dog */
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
 
-    return (new_dog);
+	return (new_dog);
 }
-
