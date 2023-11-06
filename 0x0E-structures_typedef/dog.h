b955b5a74794270fc8327d3ceca3e7dadb6619d1
RowanAshour <rokamino@hotmail.com>
2023-11-06 12:38:02 -0800

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void print_dog(struct dog *);
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
 
 #endif /* DOG_H */
