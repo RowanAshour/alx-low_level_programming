6b233de77f7a0ee19489adc09dd59942d4a52dc9
RowanAshour <rokamino@hotmail.com>
2023-11-06 13:49:25 -0800

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,71 @@
-#include <stdio.h>
+#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-#include "dog.h"
 
 /**
- * new_dog - Creates a new dog with a copy of name and owner.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- * Return: A pointer to the new dog, or NULL on failure.
+ * _strdup - returns a pointer to a new string which is a duplicate of the string str
+ * @str: string to duplicate
+ * Return: pointer to duplicated string in memory, NULL if insufficient memory
+ */
+char *_strdup(char *str)
+{
+    char *duplicate_str;
+    int i = 0, len = 0;
+
+    if (str == NULL) /* validate str input */
+        return (NULL);
+
+    while (*(str + len))
+        len++;
+    duplicate_str = malloc(sizeof(char) * (len + 1)); /* allocate memory */
+
+    if (duplicate_str == NULL) /* validate memory */
+        return (NULL);
+
+    while (i < len)
+    {
+        *(duplicate_str + i) = *(str + i);
+        i++;
+    }
+
+    duplicate_str[i] = '\0'; /* null terminate the string */
+
+    return (duplicate_str);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to new dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+    dog_t *doggo;
 
-	/* Allocate memory for the new dog */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
+    doggo = malloc(sizeof(dog_t)); /* allocate memory for new dog */
+    if (doggo == NULL) /* validate memory */
+        return (NULL);
 
-	/* Allocate memory and copy the name */
-	name_copy = strdup(name);
-	if (name_copy == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
+    doggo->name = _strdup(name); /* duplicate name and assign to dog name */
+    if (doggo->name == NULL) /* validate name */
+    {
+        free(doggo); /* prevent memory leak */
+        return (NULL);
+    }
 
-	/* Allocate memory and copy the owner */
-	owner_copy = strdup(owner);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
-		free(new_dog);
-		return (NULL);
-	}
+    doggo->age = age; /* assign age */
 
-	/* Initialize the fields of the new dog */
-	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner = owner_copy;
+    doggo->owner = _strdup(owner); /* duplicate owner and assign to dog owner */
+    if (doggo->owner == NULL) /* validate owner */
+    {
+        free(doggo->name); /* prevent memory leak */
+        free(doggo); /* prevent memory leak */
+        return (NULL);
+    }
 
-	return (new_dog);
+    return (doggo);
 }
 
