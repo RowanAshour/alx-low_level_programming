3acec6dccad712a413981c9ce544a294796931ef
RowanAshour <rokamino@hotmail.com>
2023-11-06 11:10:36 -0800

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the file it was compiled from
+ *
+ * Return: 0 if successful, other value otherwise
+ */
 int main(void)
 {
-  printf("%s\n", __FILE__);
-  return (0);
+	/* Print the name of the file */
+	printf("%s\n", __FILE__);
+	
+	/* End program successfully */
+	return (0);
 }
