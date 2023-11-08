d4f158a148baadb47fa55b42a1f797b45fbac1f4
RowanAshour <rokamino@hotmail.com>
2023-11-08 07:51:41 -0800

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  *  * print_name - prints a name
  *   * @name: name of the person
- *    * @f: function pointer to the printing function
+ *    * @f: pointer to the function that prints the name
  *     *
  *      * Return: Nothing.
  *       */
