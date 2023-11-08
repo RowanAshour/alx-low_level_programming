7c535773f50b898835541aa92579162c0f203e90
RowanAshour <rokamino@hotmail.com>
2023-11-08 10:10:28 -0800

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /*
- *  * print_name - prints a name using a function pointer
+ *  * print_name : prints a name using a function pointer
  *   * @name: name to print
  *    * @f: pointer to a function that prints a name
  *     *
