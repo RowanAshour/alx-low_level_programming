2b789eb84f58a3f7e0597ea2ec7b86fe5a321e80
RowanAshour <rokamino@hotmail.com>
2023-11-08 12:40:36 -0800

    1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
- #include "function_pointers.h"
+#include "function_pointers.h"
 
 /**
  *  * int_index - Searches for an integer in an array.
@@ -12,20 +12,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || cmp == NULL || size <= 0)
-				{
-				return (-1);
-				}
+int i;
 
-		int i;
+	if (array == NULL || cmp == NULL || size <= 0)
+		{
+			return (-1);
+		}
 
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-					{
+			{
 				return (i);
-					}
+			}
 		}
 
-	return (-1);
+		return (-1);
 }
