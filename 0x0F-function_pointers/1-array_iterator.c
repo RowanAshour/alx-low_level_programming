c056faa1a42ebd4f16dc0b1c9f5e88cf7e3f4a13
RowanAshour <rokamino@hotmail.com>
2023-11-08 10:26:57 -0800

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,52 +1,22 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- *  * array_iterator - Executes a given action on each element of an array
- *   * @array: The array to iterate through
- *    * @size: The size of the array
- *     * @action: The function to execute on each array element
- *      */
+ *  * array_iterator - Executes a function on each element of an array.
+ *   * @array: The array to operate on.
+ *    * @size: The size of the array.
+ *     * @action: A pointer to the function to be executed on each element.
+ *      *
+ *       * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-		if (array && action)
+	if (array != NULL && action != NULL)
+			{
+			size_t i;
+
+				for (i = 0; i < size; i++)
 				{
-							size_t i;
-
-									for (i = 0; i < size; i++)
-												{
-																action(array[i]);
-																		}
-										}
-}
-
-/**
- *  * print_elem - Prints an integer element
- *   * @elem: The element to print
- *    */
-void print_elem(int elem)
-{
-		printf("%d\n", elem);
-}
-
-/**
- *  * print_elem_hex - Prints an integer element in hexadecimal format
- *   * @elem: The element to print
- *    */
-void print_elem_hex(int elem)
-{
-		printf("0x%x\n", elem);
-}
-
-/**
- *  * main - Entry point of the program
- *   *
- *    * Return: 0 on success
- *     */
-int main(void)
-{
-		int array[5] = {0, 98, 402, 1024, 4096};
-
-			array_iterator(array, 5, &print_elem);
-				array_iterator(array, 5, &print_elem_hex);
-					return (0);
+					action(array[i]);
+				}
+			}
 }
