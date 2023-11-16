f0138d555d201b5c6d929c01aebbba8194823e9b
RowanAshour <rokamino@hotmail.com>
2023-11-16 04:59:03 -0800

    2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <string.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -21,6 +23,8 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+size_t custom_strlen(const char *s);
 
 #endif 
 
