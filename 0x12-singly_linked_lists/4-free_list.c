3c9d8d113baef4f71233018260e6882bf665abb0
RowanAshour <rokamino@hotmail.com>
2023-11-16 05:09:55 -0800

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *temp;
+
+	current = head;
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current->str);
+		free(current);
+		current = temp;
+	}
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,6 +27,7 @@ list_t *add_node(list_t **head, const char *str);
 size_t custom_strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif 
 
