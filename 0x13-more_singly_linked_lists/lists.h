bbac674758e849439ff7b32d2eac1d8ad10a0ee7
RowanAshour <rokamino@hotmail.com>
2023-11-22 04:14:26 -0800

    r

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,14 +10,16 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct listint_s
+struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+};
+typedef struct listint_s listint_t;
 
 /* Function prototype */
 size_t print_listint(const listint_t *h);
+listint_t *find_listint_loop(listint_t *head);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -31,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listint_t *node = find_listint_loop(head);
+
 
 #endif /* LISTS_H */
 
