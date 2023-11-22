727c747e3fa32e64b9aee14da9bf36e7f6557b93
RowanAshour <rokamino@hotmail.com>
2023-11-22 04:04:43 -0800

    0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,12 +32,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *node = find_listint_loop(head);
-struct listint_s
-{
-    int n;
-    struct listint_s *next;
-};
 
-typedef struct listint_s listint_t;
 #endif /* LISTS_H */
 
