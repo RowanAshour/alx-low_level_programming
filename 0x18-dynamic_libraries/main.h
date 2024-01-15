789a01c900bf409a52fac27091dffa4d6aa5aa13
RowanAshour <rokamino@hotmail.com>
2024-01-15 08:22:59 -0800

    0

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ *
+ * Return: On success, the number of characters written.
+ * On error, -1 is returned.
+ */
+int _putchar(char c);
+
+/**
+ * _islower - Checks if a character is a lowercase letter.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is a lowercase letter, 0 otherwise.
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - Checks if a character is an alphabetic character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is an alphabetic character, 0 otherwise.
+ */
+int _isalpha(int c);
+
+/* Other function prototypes go here */
+
+#endif /* MAIN_H */
+
